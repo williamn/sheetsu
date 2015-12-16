d955220fcb147cf99a4aff6038a0ad38cf2b0312
William Notowidagdo <wnotowidagdo@gmail.com>
2015-12-16 18:02:25 +0700

    make Sheetsu::Client::get works to returns array of all rows from spreadsheet

--- lib/sheetsu/client.rb
+++ lib/sheetsu/client.rb
@@ -1,7 +1,15 @@
+require 'httparty'
+
 module Sheetsu
   class Client
-    def self.get
-      []
+    include HTTParty
+
+    def initialize(id)
+      self.class.base_uri "sheetsu.com/apis/#{id}"
+    end
+
+    def get
+      self.class.get('/')
     end
   end
 end
--- spec/sheetsu_spec.rb
+++ spec/sheetsu_spec.rb
@@ -6,8 +6,14 @@ describe Sheetsu do
   end
 
   it 'return array of all rows from spreadsheet' do
-    client = Sheetsu::Client.new
+    client = Sheetsu::Client.new('ebe5b0fa')
     response = client.get
-    expect(response.body).to eql([])
+    expect(JSON.parse(response.body)).to eql({"status"=>200, "success"=>true, "result"=>[]})
+  end
+
+  it 'return API not found when use an invalid ID' do
+    client = Sheetsu::Client.new('foo')
+    response = client.get
+    expect(response.code).to eql(404)
   end
 end
