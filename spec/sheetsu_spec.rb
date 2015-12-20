2a45872598f4ebb578a4dc2c1d8c9fd66d26cdc2
William Notowidagdo <wnotowidagdo@gmail.com>
2015-12-20 16:31:18 +0700

    refactored spec and added spec to test Sheetsu::Client.create

--- spec/sheetsu_spec.rb
+++ spec/sheetsu_spec.rb
@@ -1,25 +1,48 @@
 require 'spec_helper'
 
 describe Sheetsu do
-  it 'return array of all rows from spreadsheet' do
-    client = Sheetsu::Client.new('foo')
-    response = client.get
-    expect(JSON.parse(response.body)).to eql('status' => 200, 'success' => true, 'result' => [])
+  describe '.get' do
+    it 'return array of all rows from spreadsheet' do
+      client = Sheetsu::Client.new('foo')
+      response = client.get
+      expect(JSON.parse(response.body)).to eql('status' => 200, 'success' => true, 'result' => [])
+    end
+
+    it 'raise error when API is not found' do
+      client = Sheetsu::Client.new('blah')
+      expect { client.get }.to raise_error('API with given URL is not found')
+    end
+
+    it 'raise error when something goes wrong' do
+      client = Sheetsu::Client.new('baz')
+      expect { client.get }.to raise_error('Something goes wrong')
+    end
   end
 
-  it 'raise error when API is not found' do
-    client = Sheetsu::Client.new('blah')
-    expect { client.get }.to raise_error('API with given URL is not found')
+  describe '.get_column' do
+    it 'returns array of all row of chosen column' do
+      client = Sheetsu::Client.new('foo')
+      response = client.get_column('Name')
+      expect(JSON.parse(response.body)).to eql('status' => 200, 'success' => true, 'result' => %w(Peter Lois Meg Chris Stewie Brian))
+    end
+
+    it 'raise error when API is not found' do
+      client = Sheetsu::Client.new('foo')
+      expect { client.get_column('baz') }.to raise_error('API with given URL is not found')
+    end
   end
 
-  it 'raise error when something goes wrong' do
-    client = Sheetsu::Client.new('baz')
-    expect { client.get }.to raise_error('Something goes wrong')
-  end
 
-  it 'returns array of all row of chosen column' do
-    client = Sheetsu::Client.new('foo')
-    response = client.get_column('Name')
-    expect(JSON.parse(response.body)).to eql('status' => 200, 'success' => true, 'result' => %w(Peter Lois Meg Chris Stewie Brian))
+  describe '.create' do
+    it 'creates a row with given values' do
+      client = Sheetsu::Client.new('foo')
+      response = client.create({ Id: '69', Name: 'Alice', Score: '9999' })
+      expect(response.code).to eql(201)
+    end
+
+    it 'raise error when API is not found' do
+      client = Sheetsu::Client.new('blah')
+      expect { client.create({ Id: '69', Name: 'Alice', Score: '9999' }) }.to raise_error('API with given URL is not found')
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,5 +50,33 @@ RSpec.configure do |config|
         status: 200,
         body: '{"status": 200, "success": true, "result": ["Peter", "Lois", "Meg", "Chris", "Stewie", "Brian"]}',
         headers: {})
+
+    stub_request(:get, 'http://sheetsu.com/apis/foo/column/baz')
+      .with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Ruby'
+        })
+      .to_return(status: 404, body: nil, headers: {})
+
+    stub_request(:post, 'http://sheetsu.com/apis/foo/')
+      .with(
+        headers: {
+          'Content-Type'=>'application/json'
+        },
+        body: '{"Id":"69","Name":"Alice","Score":"9999"}')
+      .to_return(
+        status: 201,
+        body: nil,
+        headers: {})
+
+    stub_request(:post, 'http://sheetsu.com/apis/blah/')
+      .with(
+        headers: {
+          'Content-Type'=>'application/json'
+        },
+        body: '{"Id":"69","Name":"Alice","Score":"9999"}')
+      .to_return(status: 404, body: nil, headers: {})
   end
 end
